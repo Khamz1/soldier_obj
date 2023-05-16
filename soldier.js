4b2cba799fa41efca561fdbdb58a0254c56890a7
Khamz <khamzzaty@mail.ru>
2023-05-16 17:44:22 +0300

    солдат создан

---
+++ soldier.js
@@ -0,0 +1,14 @@
+const soldier = {
+    name: 'Jane',
+    health: 10,
+    weapon: {
+        weaponName: 'm14',
+        quntity: 30
+    },
+    supplies: 3,
+    shoot(){},
+    reload(){},
+    hurt(){}
+
+
+}
