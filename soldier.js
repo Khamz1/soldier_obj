a980892e3c54d72d636db53fa8a3d4b9cf753e2c
Khamz <khamzzaty@mail.ru>
2023-05-16 18:49:13 +0300

    последняя правка

--- soldier.js
+++ soldier.js
@@ -6,21 +6,23 @@ const soldier = {
         quntity: 30
     },
     supplies: 3,
-    shoot(){
-      this.weapon.quantity-- 
-    console.log(`bah bah`)
-    if(this.weapon.quntity==0){console.log('Обойма пуста.Перезарядитесь')}
+    shoot() {
+        this.weapon.quantity--
+        console.log("bah bah")
+        if (this.weapon.quantity == 0) { return console.log('Обойма пуста.Перезарядитесь') }
 
     },
-    reload(){
-        this.weapon.quantity=30
+    reload() {
+        this.weapon.quantity = 30
         this.supplies--
         console.log('перезарядка')
-        if(this.supplies==0){console.log("не осталось припасов")}
-    
+        if (this.supplies == 0) { return console.log("не осталось припасов") }
+
     },
-    hurt(){this.health--
-    if(this.health==0){console.log("ты проиграл")}}
+    hurt() {
+        this.health--
+        if (this.health <= 0) { return console.log("ты проиграл") }
+    }
 
 
 }
