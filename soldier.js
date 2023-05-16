e0bb647d4f67638aac1cf89598d65b9f1f7edc3c
Khamz <khamzzaty@mail.ru>
2023-05-16 18:28:21 +0300

    добавил солдату действия

--- soldier.js
+++ soldier.js
@@ -6,9 +6,22 @@ const soldier = {
         quntity: 30
     },
     supplies: 3,
-    shoot(){},
-    reload(){},
-    hurt(){}
+    shoot(){
+      this.weapon.quantity-- 
+    console.log(`bah bah`)
+    if(this.weapon.quntity==0){console.log('Обойма пуста.Перезарядитесь')}
+
+    },
+    reload(){
+        this.weapon.quantity=30
+        this.supplies--
+        console.log('перезарядка')
+        if(this.supplies==0){console.log("не осталось припасов")}
+    
+    },
+    hurt(){this.health--
+    if(this.health==0){console.log("ты проиграл")}}
 
 
 }
+
